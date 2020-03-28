9af80e202840e35fd6ce143e957f4d9296069758
sukrit sangvong <sukritsangvong@eduroam-163-3.dyn.carleton.edu>
2020-03-28 16:04:39 -0500

    a bit of demographic part added

--- caseLoads.R
+++ caseLoads.R
@@ -1,4 +1,5 @@
 install.packages("ggplot2")
+install.packages("tidyverse")
 install.packages("dplyr")
 install.packages("MASS")
 install.packages("gridExtra")
@@ -45,5 +46,39 @@ p2 <- ggplot(df.dockets_district, aes(x = dockets_district, y = ratioTo100000)) 
 grid.arrange(p1, p2, nrow = 1)
 
 
+#dealing with demographoic
+#preparing the FIPS data
 
+train_dockets$highOrLow <- -1
+
+for(i in 1:dim(df.dockets_district)[1]){
+  for(j in 1:dim(train_dockets)[1]){
+    if (train_dockets$district[j] == df.dockets_district$dockets_district[i]){
+      train_dockets$highOrLow[j] <- df.dockets_district$highOrLow[i]
+    }
+  }
+}
+
+df.dockets_FIPS_HIGH %>% filter(train_dockets, highOrLow == 1)
+df.dockets_FIPS_LOW <- filter(train_dockets, highOrLow == 0)
+df.dockets_FIPS_HIGH <- select_(df.dockets_FIPS_HIGH, "district", "filers_county")
+df.dockets_FIPS_LOW <- select_(df.dockets_FIPS_LOW, "district", "filers_county")
+
+#dealing with high filing
+high.frequency <- table(df.dockets_FIPS_HIGH$filers_county)
+df.high_FIPS <- as.data.frame(high.frequency)
+
+#dealing with low filing
+low.frequency <- table(df.dockets_FIPS_LOW$filers_county)
+df.low_FIPS <- as.data.frame(low.frequency)
+
+#plotting
+p3 <- ggplot(df.high_FIPS, aes(x = Var1, y = Freq)) +
+  geom_bar(stat = "identity") + ggtitle("High Filing FIPS' Frequencies") +
+  xlab("FIPS") + ylab("Frequency")
+p4 <- ggplot(df.low_FIPS, aes(x = Var1, y = Freq)) +
+  geom_bar(stat = "identity") + ggtitle("Low Filing FIPS' Frequencies") +
+  xlab("FIPS") + ylab("Frequency")
+
+grid.arrange(p3, p4, nrow = 1)
 
