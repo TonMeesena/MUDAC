ae2eb398eba876a7dd256aba15ce5ab95b2931b0
sukrit sangvong <sukritsangvong@eduroam-163-3.dyn.carleton.edu>
2020-03-28 11:39:39 -0500

    caseLoad histrogram

--- caseLoads.R
+++ caseLoads.R
@@ -1,3 +1,12 @@
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("MASS")
+
+# Library
+library(ggplot2)
+library(dplyr)
+library(MASS)   
+
 train_dockets <- read.csv("raw_data/train_dockets.csv",header=TRUE)
 train_other <- read.csv("raw_data/train_other_motions.csv",header=TRUE)
 train_terminating <- read.csv("raw_data/train_terminating_motions.csv",header=TRUE)
@@ -6,4 +15,8 @@ test_dockets <- read.csv("raw_data/test_dockets.csv",header=TRUE)
 test_other <- read.csv("raw_data/test_other_motions.csv",header=TRUE)
 test_terminating <- read.csv("raw_data/test_terminating_motions.csv",header=TRUE)
 
+dockets_district <- train_dockets$district
+dockets_district.frequency <- table(dockets_district)
+barplot(dockets_districts.frequency)
 
+ggplot(train_dockets, aes(district)) + geom_histogram()
