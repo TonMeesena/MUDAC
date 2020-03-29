32e2d6c92a1c8d2172fa8cd9d0cbe1413fe037c5
sukrit sangvong <sukritsangvong@eduroam-163-3.dyn.carleton.edu>
2020-03-29 00:55:45 -0500

    demographic stuff

--- caseLoads.R
+++ caseLoads.R
@@ -3,12 +3,15 @@ install.packages("tidyverse")
 install.packages("dplyr")
 install.packages("MASS")
 install.packages("gridExtra")
+install.packages("corrplot")
 
 # Library
 library(ggplot2)
 library(dplyr)
 library(MASS)   
 library(gridExtra)
+library(forcats)
+library(corrplot)
 
 
 #loading data
@@ -46,7 +49,6 @@ p2 <- ggplot(df.dockets_district, aes(x = dockets_district, y = ratioTo100000)) 
   geom_bar(stat = "identity") + 
   geom_col(fill = ifelse(df.dockets_district$highOrLow >= 1, "lightsalmon2", "gray39")) + 
   ggtitle("Districts' Ratio Over 100,000") + xlab("Districts") + ylab("Ratio Over 100,000")
-
 grid.arrange(p1, p2, nrow = 1)
 
 
@@ -54,7 +56,6 @@ grid.arrange(p1, p2, nrow = 1)
 #preparing the FIPS data
 
 train_dockets$highOrLow <- -1
-
 for(i in 1:dim(df.dockets_district)[1]){
   for(j in 1:dim(train_dockets)[1]){
     if (train_dockets$district[j] == df.dockets_district$dockets_district[i]){
@@ -78,10 +79,10 @@ df.low_FIPS <- as.data.frame(low.frequency)
 
 
 #plotting
-p3 <- ggplot(df.high_FIPS, aes(x = Var1, y = Freq)) +
+p3 <- ggplot(df.high_FIPS, aes(x = fct_reorder(Var1, desc(Freq)), y = Freq)) +
   geom_bar(stat = "identity") + ggtitle("High Filing FIPS' Frequencies") +
   xlab("FIPS") + ylab("Frequency")
-p4 <- ggplot(df.low_FIPS, aes(x = Var1, y = Freq)) +
+p4 <- ggplot(df.low_FIPS, aes(x = fct_reorder(Var1,desc(Freq)), y = Freq)) +
   geom_bar(stat = "identity") + ggtitle("Low Filing FIPS' Frequencies") +
   xlab("FIPS") + ylab("Frequency")
 
@@ -89,14 +90,45 @@ grid.arrange(p3, p4, nrow = 1)
 
 #modify for demographic
 
-df.full_FIPS <- arrange(df.dockets_district, desc(ratioTo100000))
-df.Ordered_dockets <- data.frame(matrix(ncol = length(train_dockets), nrow = 0))
-df.Ordered_dockets <- subset(train_dockets, FALSE)
+df.full_FIPS <- select_(train_dockets, "filers_county", "highOrLow")
+df.freq_tmp <- as.data.frame(table(df.full_FIPS))
 
-for(i in 1:dim(df.full_FIPS)[1]){
-  for(j in 1:dim(train_dockets)[1]){
-    if (train_dockets$district[j] == df.full_FIPS$dockets_district[i]){
-      df.Ordered_dockets %>% rbind(train_dockets[j,])
-    }
+#cleaning modified data
+df.freq_full_FIPS <- data.frame(matrix(ncol=3, nrow=0))
+name <- c("filers_county", "highOrLow", "Freq")
+colnames(df.freq_full_FIPS) <- name
+
+df.freq_tmp[2,1:3]
+for (i in 1:dim(df.freq_tmp)[1]){
+  if (df.freq_tmp$Freq[i] > 0) {
+    df.freq_full_FIPS <- rbind(df.freq_full_FIPS, df.freq_tmp[i,1:3])
   }
 }
+
+#getting rid of repetitions
+df.freq_full_FIPS <- df.freq_full_FIPS %>% distinct(filers_county, .keep_all = TRUE)
+
+#joining data with demographic
+acs2015_county <- acs2015_county %>% mutate(CensusId = as.factor(CensusId))
+df.freq_full_FIPS <-left_join(df.freq_full_FIPS, acs2015_county, by=c("filers_county" = "CensusId"))
+
+#Adding data
+df.freq_full_FIPS$MaleFemaleRatio <- df.freq_full_FIPS$Men/df.freq_full_FIPS$Women 
+
+#Visualizing data
+pd1 <- ggplot(df.freq_full_FIPS, aes(x = MaleFemaleRatio, y = highOrLow)) +
+  geom_point() + 
+  ggtitle("Demo1") + xlab("Ratio of Male VS Female") + ylab("high or low")
+pd1
+
+pd2 <- ggplot(df.freq_full_FIPS, aes(x = Unemployment, y = highOrLow)) +
+  geom_point() + 
+  ggtitle("Demo1") + xlab("Unemployment Rate") + ylab("high or low")
+pd2
+
+pd3 <- ggplot(df.freq_full_FIPS, aes(x = Poverty, y = highOrLow)) +
+  geom_bar(stat = "identity") + 
+  ggtitle("Demo1") + xlab("Poverty") + ylab("high or low")
+pd3
+
+
